41bdcac0c6ff005c0e75213fc3593a35d9250d59
Woke <66000596+woke999@users.noreply.github.com>
2021-07-11 12:48:58 -0700

    Add files via upload

---
+++ Handler/Discord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordRPC;
+
+namespace DaysGoneRPC.Handler
+{
+    public static class Discord
+    {
+        public static DiscordRpcClient client;
+
+        public static void Start()
+        {
+            client = new DiscordRpcClient("863866927013298187");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = $"Days Gone",
+                State = $"Exploring Oregon", 
+                Assets = new Assets()
+                {
+                    LargeImageKey = "days_gone",
+                    LargeImageText = "Days Gone",
+                }
+            });
+        }
+
+        public static void ShutDown()
+        {
+            client.Dispose();
+        }
+    }
+}
